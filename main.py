f09188173d0521c5c57b7cac1242d555093f8a50
hugostubler-artemis <hugo.stubler@artemistechnologies.co.uk>
2024-08-02 16:05:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if st.button('Finish line'):
     
     if st.session_state['timestamp1'] and st.session_state['timestamp2'] and st.session_state['timestamp3'] and st.session_state['timestamp4'] and st.session_state['timestamp5']:
             
-            ####   st.session_state['timestamp1'] = (st.session_state['timestamp1'] - timedelta(hours=2)).replace(tzinfo=None)
+            st.session_state['timestamp1'] = (st.session_state['timestamp1'] - timedelta(hours=2)).replace(tzinfo=None)
             #st.session_state['timestamp2'] = (st.session_state['timestamp2'] - timedelta(hours=2)).replace(tzinfo=None)
             #st.session_state['timestamp3'] = (st.session_state['timestamp3'] - timedelta(hours=2)).replace(tzinfo=None)
             #st.session_state['timestamp4'] = (st.session_state['timestamp4'] - timedelta(hours=2)).replace(tzinfo=None)
